9c72997358870b7ef76a2facf395d18aec0d6b32
Gavin Price <gaprice@lbl.gov>
2025-07-02 21:17:16 -0700

    Add basic client operations
    Constructor, request method with error checking, check url and token are
    valid

--- test/test_client.py
+++ test/test_client.py
@@ -1,9 +1,77 @@
+import pytest
+
 from conftest import CTS_URL, auth_user
-import requests
+
+from cdmtaskserviceclient.client import CTSClient, UnexpectedServerResponseError, InvalidTokenError
 
 
-def test_whoami(auth_user):
-    # TODO test make the client do this
-    res = requests.get(f"{CTS_URL}/whoami", headers={"Authorization": f"Bearer {auth_user[1]}"})
-    assert res.json() == {"user": auth_user[0], "is_service_admin": False}
+def test_constructor(auth_user):
+    cli = CTSClient(f"   \t{auth_user[1]}   ", url=f"   {CTS_URL}\t   ")
+    assert cli.user == auth_user[0]
 
+
+def test_constructor_fail_no_token():
+    for t in [None, "  \t  "]:
+        with pytest.raises(ValueError) as e:
+            CTSClient(t)
+        assert str(e.value) == (
+            "The token argument is required and cannot be a whitespace only string"
+        )
+
+
+def test_constructor_fail_no_url():
+    for u in [None, "  \t  "]:
+        with pytest.raises(ValueError) as e:
+            CTSClient("token", url=u)
+        assert str(e.value) == (
+            "The url argument is required and cannot be a whitespace only string"
+        )
+
+
+def test_constructor_fail_error_5XX_response():
+    with pytest.raises(UnexpectedServerResponseError) as e:
+        CTSClient("token", url="https://ci.kbase.us/services/ws")
+    assert str(e.value) == "Error response (500) from the CTS"
+
+
+# this doesn't work
+# def test_constructor_fail_error_3XX_response():
+#     with pytest.raises(UnexpectedServerResponseError) as e:
+#         CTSClient("token", url="https://neverssl.com")
+#     assert str(e.value) == ("Error response (500) from the CTS")
+
+
+def test_constructor_fail_error_4XX_response_not_json(auth_user):
+    with pytest.raises(UnexpectedServerResponseError) as e:
+        CTSClient("token", url="https://ci.kbase.us/services/foo")
+    assert str(e.value) == "Unparseable error response (404) from the CTS"
+
+
+def test_constructor_fail_error_unexpected_error_code(auth_user):
+    with pytest.raises(UnexpectedServerResponseError) as e:
+        # token gets checked before endpoint resolution
+        CTSClient(auth_user[1], url=CTS_URL + "/fake")
+    assert str(e.value) == (
+        "Unexpected error code (40000) from the server: Not Found"
+    )
+
+
+def test_constructor_fail_bad_token():
+    with pytest.raises(InvalidTokenError) as e:
+        CTSClient("bad_token", url=CTS_URL)
+    assert str(e.value) == "The authorization token is invalid"
+
+
+def test_constructor_fail_success_response_not_json():
+    with pytest.raises(UnexpectedServerResponseError) as e:
+        CTSClient("token", url="https://example.com")
+    assert str(e.value) == "Unparseable success response from the CTS"
+
+
+def test_constructor_fail_service_not_cts():
+    with pytest.raises(ValueError) as e:
+        CTSClient("token", url="https://ci.kbase.us/services/groups")
+    assert str(e.value) == (
+        "The CTS url https://ci.kbase.us/services/groups "
+        + "does not appear to point to the CTS service"
+    )
